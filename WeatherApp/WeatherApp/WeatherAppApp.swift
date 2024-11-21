f722ef8b64663532fecf5755c563017abd8a1840
Caleb McKinley <calebmckinley@dhcp-10-132-223-255.uawifi.arizona.edu>
2024-11-21 13:31:48 -0700

    Add Xcode and SwiftUI model

---
+++ WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Caleb McKinley on 11/21/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
