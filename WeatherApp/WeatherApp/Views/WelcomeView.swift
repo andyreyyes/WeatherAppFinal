b5912872016aac46a84f99173c9abc47d588c731
John Le <john@Johns-MacBook-Pro.local>
2024-11-21 22:29:12 -0700

    intial changes

---
+++ WeatherApp/WeatherApp/Views/WelcomeView.swift
@@ -0,0 +1,20 @@
+//
+//  WelcomeView.swift
+//  WeatherApp
+//
+//  Created by John Le on 11/21/24.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct WelcomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeView()
+    }
+}
